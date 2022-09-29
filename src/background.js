089f557f21f238856ed2f536fa3ad800c4bb04b3
izidoiro <izidoriojr@gmail.com>
2022-09-29 14:14:09 -0300

    chore

--- src/background.js
+++ src/background.js
@@ -1,116 +1,128 @@
-'use strict'
+"use strict";
 
-import { app, protocol, BrowserWindow, globalShortcut, nativeImage } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import {
+  app,
+  protocol,
+  BrowserWindow,
+  globalShortcut,
+  nativeImage,
+  ipcMain,
+} from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== "production";
 
-app.setAppUserModelId('me.ibento.clipping')
+app.setAppUserModelId("me.ibento.clipping");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
+  { scheme: "app", privileges: { secure: true, standard: true } },
+]);
 
 function createWindow() {
-  const icon = nativeImage.createFromPath(`${app.getAppPath()}/build/icon.png`)
+  const icon = nativeImage.createFromPath(`${app.getAppPath()}/build/icon.png`);
   if (app.dock) {
-    app.dock.setIcon(icon)
+    app.dock.setIcon(icon);
   }
   // Create the browser window.
   win = new BrowserWindow({
     icon,
     width: 600,
     height: 812,
+    devTools: true,
     //frame: false,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+    },
+  });
 
-  win.setMenuBarVisibility(false)
+  win.setMenuBarVisibility(false);
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
     //if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
-    createProtocol('app')
+    createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    win.loadURL("app://./index.html");
   }
 
-  win.webContents.on('did-finish-load', () => {
-    const { title, version} = require('../package.json')
-    win.setTitle(`${title} v ${version}`)
-  })
+  win.webContents.on("did-finish-load", () => {
+    const { title, version } = require("../package.json");
+    win.setTitle(`${title} v ${version}`);
 
-  win.on('closed', () => {
-    win = null
-  })
+    ipcMain.handle("scrap", async (_, request) => {
+      console.log({ request });
+
+      return { status: 200, data: {} };
+    });
+  });
+
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
-function createShortcuts(){
-  globalShortcut.register('CmdOrCtrl+J', toogleDevTools)
+function createShortcuts() {
+  globalShortcut.register("CmdOrCtrl+J", toogleDevTools);
 }
 
-function toogleDevTools(){
-  win.webContents.toggleDevTools()
+function toogleDevTools() {
+  win.webContents.toggleDevTools();
 }
-
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS)
+      await installExtension(VUEJS_DEVTOOLS);
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  createWindow()
-  createShortcuts()
-})
+  createWindow();
+  createShortcuts();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
-
--- src/components/FormNews.vue
+++ src/components/FormNews.vue
@@ -1,240 +1,258 @@
 <template>
-    <b-modal id="bv-modal-clipping" centered hide-footer >
-        <template v-slot:modal-title>
-            {{form.id ? 'Editar notícia': 'Incluir notícia'}}
-        </template>
-        <form @submit.prevent="onSubmit">
-            <div class="form-group col-12">
-                <label>Url</label>
-                <b-form-input
-                        type="url"
-                        v-model="form.url"
-                        required />
+  <b-modal id="bv-modal-clipping" centered hide-footer>
+    <template v-slot:modal-title>
+      {{ form.id ? "Editar notícia" : "Incluir notícia" }}
+    </template>
+    <form @submit.prevent="onSubmit">
+      <div class="form-group col-12">
+        <label>Url</label>
+        <b-form-input type="url" v-model="form.url" required />
+      </div>
+      <div class="form-group col-12">
+        <label>Url encurtada</label>
+        <div class="input-group mb-2">
+          <b-form-input type="url" v-model="form.urlShort" />
+          <div
+            class="input-group-append"
+            v-b-tooltip.hover
+            title="Encurtar a URL"
+          >
+            <div
+              class="input-group-text"
+              :class="[form.url && bitlyToken ? 'scrap-btn' : '']"
+              @click="onUrlShort"
+            >
+              <b-overlay
+                :show="showLink"
+                rounded
+                opacity="0.6"
+                spinner-small
+                spinner-variant="primary"
+                class="d-inline-block"
+              >
+                <i class="fa fa-magic"></i>
+              </b-overlay>
             </div>
-            <div class="form-group col-12">
-                <label>Url encurtada</label>
-                <div class="input-group mb-2">
-                    <b-form-input
-                            type="url"
-                            v-model="form.urlShort"
-                             />
-                    <div class="input-group-append" v-b-tooltip.hover title="Encurtar a URL">
-                        <div class="input-group-text" :class="[(form.url && bitlyToken) ? 'scrap-btn' : '']" @click="onUrlShort">
-
-                            <b-overlay
-                                    :show="showLink"
-                                    rounded
-                                    opacity="0.6"
-                                    spinner-small
-                                    spinner-variant="primary"
-                                    class="d-inline-block"
-                            >
-                                <i class="fa fa-magic"></i>
-                            </b-overlay>
-                        </div>
-                    </div>
-                </div>
+          </div>
+        </div>
+      </div>
+      <div class="form-group col-12">
+        <label>Resumo</label>
+        <div class="input-group">
+          <b-form-textarea
+            id="textarea"
+            v-model="form.description"
+            placeholder="Entre com um resumo da notícia..."
+            rows="3"
+            max-rows="6"
+            required
+          ></b-form-textarea>
+          <div
+            class="input-group-append"
+            v-b-tooltip.hover
+            title="Scrape do Título e Descrição do link"
+          >
+            <div
+              class="input-group-text"
+              :class="[form.url && bitlyToken ? 'scrap-btn' : '']"
+              @click="onScrape"
+            >
+              <b-overlay
+                :show="showScrap"
+                rounded
+                opacity="0.6"
+                spinner-small
+                spinner-variant="primary"
+                class="d-inline-block"
+              >
+                <i class="fa fa-magic"></i>
+              </b-overlay>
             </div>
-            <div class="form-group col-12">
-                <label>Resumo</label>
-                <div class="input-group">
-                    <b-form-textarea
-                            id="textarea"
-                            v-model="form.description"
-                            placeholder="Entre com um resumo da notícia..."
-                            rows="3"
-                            max-rows="6"
-                            required
-                    ></b-form-textarea>
-                    <div class="input-group-append" v-b-tooltip.hover title="Scrape do Título e Descrição do link">
-                        <div class="input-group-text" :class="[(form.url && bitlyToken) ? 'scrap-btn' : '']" @click="onScrape">
+          </div>
+        </div>
+      </div>
 
-                            <b-overlay
-                                    :show="showScrap"
-                                    rounded
-                                    opacity="0.6"
-                                    spinner-small
-                                    spinner-variant="primary"
-                                    class="d-inline-block"
-                            >
-                                <i class="fa fa-magic"></i>
-                            </b-overlay>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <btn-submit :busy="busy"/>
-        </form>
-    </b-modal>
+      <btn-submit :busy="busy" />
+    </form>
+  </b-modal>
 </template>
 
 <script>
-    import api from '../utils/api';
-    import session from "../utils/session";
-    import BtnSubmit from "./Submit";
-    import { convertHTMLEntity, clearObj } from '../utils/helper'
+import { ipcRender } from "electron";
+import api from "../utils/api";
+import session from "../utils/session";
+import BtnSubmit from "./Submit";
+import { convertHTMLEntity, clearObj } from "../utils/helper";
 
-    import { BitlyClient } from 'bitly';
+import { BitlyClient } from "bitly";
 
+async function short(url, token) {
+  const bitly = new BitlyClient(token, {});
+  try {
+    return await bitly.shorten(url);
+  } catch (e) {
+    console.log("error", e);
+    return null;
+  }
+}
 
-    async function short(url, token) {
-        const bitly = new BitlyClient( token, {});
-        try {
-            return await bitly.shorten(url);
-        }catch (e) {
-            console.log('error', e)
-            return null
-        }
+export default {
+  name: "FormNews",
+  components: { BtnSubmit },
+  props: {
+    form: {
+      type: Object,
+      default: () => ({
+        id: "",
+        url: "",
+        urlShort: "",
+        description: "",
+      }),
+    },
+  },
+  data() {
+    return {
+      busy: false,
+      showLink: false,
+      showScrap: false,
+      bitlyToken: "",
+      preferences: "",
+      errors: {},
+    };
+  },
+  mounted() {
+    //
+  },
+  methods: {
+    show() {
+      this.preferences = session.getObject("preferences");
+      if (!this.preferences) {
+        this.$bvToast.toast(
+          "Para começar a usar o clipping Você precisa salvar os parâmetros de configuração. Acesse o Menu/Preferência",
+          {
+            title: "Error",
+            variant: "danger",
+            toaster: "b-toaster-bottom-right",
+            autoHideDelay: 5000,
+          }
+        );
+        return;
+      }
 
-    }
+      this.$bvModal.show("bv-modal-clipping");
+    },
+    onSubmit() {
+      if (this.form.id === undefined || this.form.id === "") {
+        const id = Date.now();
+        this.$store
+          .dispatch("clipping/addNews", { ...this.form, id })
+          .then(() => {
+            this.onReset();
+          });
+      } else {
+        this.$store.dispatch("clipping/editNews", this.form).then(() => {
+          this.onReset();
+          this.$bvModal.hide("bv-modal-clipping");
+        });
+      }
+    },
+    onReset() {
+      clearObj(this.form);
+    },
+    async onScrape() {
+      if (this.form.url === undefined || this.form.url === "") {
+        this.$bvToast.toast("Você precisa informar uma url válida", {
+          title: "Error",
+          variant: "danger",
+          autoHideDelay: 5000,
+        });
+        return;
+      }
 
-    export default {
-        name: "FormNews",
-        components: { BtnSubmit },
-        props: {
-            form: {
-                type: Object,
-                default: () => ({
-                    id: '',
-                    url: '',
-                    urlShort: '',
-                    description: ''
-                })
+      this.showScrap = true;
+      const url =
+        "https://jovempan.com.br/noticias/brasil/mega-sena-acumula-e-deve-pagar-r-300-milhoes-no-sabado.html";
+
+      //   ipcRender
+      //     .invoke("scrap", { url })
+      //     .then((response) => console.log({ response }));
+
+      api
+        .get(`${this.preferences.proxy}/${this.form.url}`)
+        .then((result) => {
+          //hack para resolver html minificados
+          let html = result.data.replace(/'/g, '"');
+          html = html.replace(/">|"\/>|" \/>/g, '">\n');
+
+          let rgxTitle = /"og:title"(.*)content="(.*)"/;
+          let rgxDesc = /"og:description"(.*)content="(.*)"/;
+
+          let title = rgxTitle.exec(html);
+          let desc = rgxDesc.exec(html);
+
+          if (title && desc) {
+            this.form.description = convertHTMLEntity(`${title[2]} ${desc[2]}`);
+          } else {
+            rgxTitle = /content="(.*)" (.*)og:title/;
+            rgxDesc = /content="(.*)" (.*)og:description/;
+            title = rgxTitle.exec(html);
+            desc = rgxDesc.exec(html);
+            this.form.description = convertHTMLEntity(`${title[1]} ${desc[1]}`);
+          }
+        })
+        .catch(() => {
+          this.$bvToast.toast(
+            "É possível que tenha atingido a quantidade de requisições por minuto. Aguarde ou insira o resumo manualmente. Acesse o github do desenvolvedor e saiba como criar um servidor Proxy CORS",
+            {
+              title: "Erro",
+              variant: "warning",
+              autoHideDelay: 5000,
             }
-        },
-        data () {
-            return {
-                busy: false,
-                showLink: false,
-                showScrap: false,
-                bitlyToken: '',
-                preferences: '',
-                errors: {}
-            }
-        },
-        mounted() {
-            //
-        },
-        methods:{
-            show (){
-                this.preferences = session.getObject('preferences')
-                if(! this.preferences){
-                    this.$bvToast.toast('Para começar a usar o clipping Você precisa salvar os parâmetros de configuração. Acesse o Menu/Preferência', {
-                        title: 'Error',
-                        variant: 'danger',
-                        toaster: 'b-toaster-bottom-right',
-                        autoHideDelay: 5000
-                    })
-                    return
-                }
+          );
+        })
+        .finally(() => {
+          this.showScrap = false;
+        });
+    },
+    async onUrlShort() {
+      if (this.form.url === undefined || this.form.url === "") {
+        this.$bvToast.toast("Você precisa informar uma url válida", {
+          title: "Error",
+          variant: "danger",
+          toaster: "b-toaster-bottom-right",
+          autoHideDelay: 5000,
+        });
+        return;
+      }
 
-                this.$bvModal.show('bv-modal-clipping')
-            },
-            onSubmit (){
-                if( this.form.id === undefined || this.form.id === '' ){
-                    const id = Date.now()
-                    this.$store.dispatch('clipping/addNews',{...this.form, id})
-                        .then( () => {
-                            this.onReset()
-                        })
-                }else{
+      if (
+        this.preferences.bitlyToken === undefined ||
+        this.preferences.bitlyToken === ""
+      ) {
+        this.$bvToast.toast(
+          "Para encurtar a url automaticamente você precisa informar o token do Bitly em: Menu/Preferências",
+          {
+            title: "Error",
+            variant: "danger",
+            toaster: "b-toaster-bottom-right",
+            autoHideDelay: 5000,
+          }
+        );
+        return;
+      }
 
-                    this.$store.dispatch('clipping/editNews', this.form)
-                        .then( () => {
-                            this.onReset()
-                            this.$bvModal.hide('bv-modal-clipping')
-                        })
-                }
-            },
-            onReset (){
-                clearObj(this.form)
-            },
-            async onScrape (){
-
-                if( this.form.url  === undefined || this.form.url === '' ){
-                    this.$bvToast.toast('Você precisa informar uma url válida', {
-                        title: 'Error',
-                        variant: 'danger',
-                        autoHideDelay: 5000
-                    })
-                    return
-                }
-
-                this.showScrap = true;
-
-                api.get(  `${this.preferences.proxy}/${this.form.url}`)
-                    .then( result => {
-
-                        //hack para resolver html minificados
-                        let html = result.data.replace(/'/g,'"');
-                        html = html.replace(/">|"\/>|" \/>/g,'">\n');
-
-                        let rgxTitle = /"og:title"(.*)content="(.*)"/;
-                        let rgxDesc = /"og:description"(.*)content="(.*)"/;
-
-                        let title =  rgxTitle.exec(html);
-                        let desc =  rgxDesc.exec(html);
-
-                        if(title && desc){
-                            this.form.description = convertHTMLEntity(`${title[2]} ${desc[2]}`)
-                        } else {
-                            rgxTitle = /content="(.*)" (.*)og:title/;
-                            rgxDesc = /content="(.*)" (.*)og:description/;
-                            title =  rgxTitle.exec(html);
-                            desc =  rgxDesc.exec(html);
-                            this.form.description = convertHTMLEntity(`${title[1]} ${desc[1]}`)
-                        }
-
-                    })
-                    .catch( ()=> {
-                        this.$bvToast.toast('É possível que tenha atingido a quantidade de requisições por minuto. Aguarde ou insira o resumo manualmente. Acesse o github do desenvolvedor e saiba como criar um servidor Proxy CORS', {
-                            title: 'Erro',
-                            variant: 'warning',
-                            autoHideDelay: 5000
-                        })
-                    })
-                    .finally( () => {
-                        this.showScrap = false;
-                    })
-            },
-            async onUrlShort (){
-
-                if( this.form.url  === undefined || this.form.url === '' ){
-                    this.$bvToast.toast('Você precisa informar uma url válida', {
-                        title: 'Error',
-                        variant: 'danger',
-                        toaster: 'b-toaster-bottom-right',
-                        autoHideDelay: 5000
-                    })
-                    return
-                }
-
-                if(this.preferences.bitlyToken === undefined || this.preferences.bitlyToken === '' ){
-                    this.$bvToast.toast('Para encurtar a url automaticamente você precisa informar o token do Bitly em: Menu/Preferências', {
-                        title: 'Error',
-                        variant: 'danger',
-                        toaster: 'b-toaster-bottom-right',
-                        autoHideDelay: 5000
-                    })
-                    return
-                }
-
-                this.showLink = true;
-                const urlShort =  await short(this.form.url, this.preferences.bitlyToken);
-                this.form.urlShort = urlShort.link;
-                this.showLink = false
-            }
-        }
-    }
-
+      this.showLink = true;
+      const urlShort = await short(this.form.url, this.preferences.bitlyToken);
+      this.form.urlShort = urlShort.link;
+      this.showLink = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .scrap-btn{
-        cursor: pointer;
-        color: #007BFF;
-    }
-
-
+.scrap-btn {
+  cursor: pointer;
+  color: #007bff;
+}
 </style>
